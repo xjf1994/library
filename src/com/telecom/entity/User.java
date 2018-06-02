4dc4afdb5959aa55a5a91bc3834bcdc2758fe34b
xjf1994 <353348949@qq.com>
2018-06-02 18:26:55 +0800

    1

---
+++ src/com/telecom/entity/User.java
@@ -0,0 +1,11 @@
+package com.telecom.entity;
+
+public class User {
+	
+	private int uid;
+	private String username;
+	private String pass;
+	private int pid;
+	private int libm;
+	private int tid;
+}
